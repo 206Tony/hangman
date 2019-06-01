0157cc761259d831054646e376729d408eb6890c
Anthony Zardis <anthonyzardis@gmail.com>
2019-06-01 14:57:24 -0700

    Generate random word

---
+++ js/script.js
@@ -0,0 +1,92 @@
+//Welcome
+ var playerName = prompt("Please Enter Your Name.");
+// alert("Hello " + playerName);
+
+// Variable declarations
+
+//Array containing all letters is alphabet
+const startingLettersArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".split(" ");  //console.log(alphabet);
+var alphabet = startingLettersArray;
+
+//Fill Array called words with strings
+var words = ["Adult","Aeroplane","Air","Aircraft Carrier","Airforce","Airport","Album","Alphabet","Apple","Arm","Army","Baby","Baby","Backpack","Balloon","Banana","Bank","Barbecue","Bathroom","Bathtub","Bed","Bed","Bee","Bible","Bible","Bird","Bomb","Book","Boss","Bottle","Bowl","Box","Boy","Brain","Bridge","Butterfly","Button","Cappuccino","Car","Car-race","Carpet","Carrot","Cave","Chair","Chess Board","Chief","Child","Chisel","Chocolates","Church","Church","Circle","Circus","Circus","Clock","Clown","Coffee","Coffee-shop","Comet","Compact Disc","Compass","Computer","Crystal","Cup","Cycle","Data Base","Desk","Diamond","Dress","Drill","Drink","Drum","Dung","Ears","Earth","Egg","Electricity","Elephant","Eraser","Explosive","Eyes","Family","Fan","Feather","Festival","Film","Finger","Fire","Floodlight","Flower","Foot","Fork","Freeway","Fruit","Fungus","Game","Garden","Gas","Gate","Gemstone","Girl","Gloves","God","Grapes","Guitar","Hammer","Hat","Hieroglyph","Highway","Horoscope","Horse","Hose","Ice","Ice-cream","Insect","Jet fighter","Junk","Kaleidoscope","Kitchen","Knife","Leather jacket","Leg","Library","Liquid","Magnet","Man","Map","Maze","Meat","Meteor","Microscope","Milk","Milkshake","Mist","Money $$$$","Monster","Mosquito","Mouth","Nail","Navy","Necklace","Needle","Onion","PaintBrush","Pants","Parachute","Passport","Pebble","Pendulum","Pepper","Perfume","Pillow","Plane","Planet","Pocket","Post-office","Potato","Printer","Prison","Pyramid","Radar","Rainbow","Record","Restaurant","Rifle","Ring","Robot","Rock","Rocket","Roof","Room","Rope","Saddle","Salt","Sandpaper","Sandwich","Satellite","School","Sex","Ship","Shoes","Shop","Shower","Signature","Skeleton","Slave","Snail","Software","Solid","Space Shuttle","Spectrum","Sphere","Spice","Spiral","Spoon","Sports-car","Spot Light","Square","Staircase","Star","Stomach","Sun","Sunglasses","Surveyor","Swimming Pool","Sword","Table","Tapestry","Teeth","Telescope","Television","Tennis racquet","Thermometer","Tiger","Toilet","Tongue","Torch","Torpedo","Train","Treadmill","Triangle","Tunnel","Typewriter","Umbrella","Vacuum","Vampire","Videotape","Vulture","Water","Weapon","Web","Wheelchair","Window","Woman","Worm","X-ray"];
+
+//word takes words Array using Math.random, and 
+//Math.floor to pick a random word from array.
+var word = words[Math.floor(Math.random() * words.length)];
+
+//answerArray starts empty, uses a
+//for loop to iterate the word so 
+//it has the correct num of blanks 
+var answerArray = [];
+for(let i = 0; i < answerArray.length; i++) {
+  answerArray[i] = "_";
+}
+
+var lettersRemaining = word.length;
+alert(lettersRemaining);
+  while (lettersRemaining > 0) {
+     //has player progress, input, 
+
+     //turns remaining, update answerArray 
+     //when selection is correct
+    return answerArray.join(" "));
+  };
+
+var player = prompt("Select a Letter");
+if !(player) {
+  return;
+} else if ()
+}
+
+var resetGame = turnsLeft + 10;
+var turnsLeft = 0;
+var turns = 10;
+
+//alert(name + " you have " + move + "remaining");
+// HTML Element References
+
+
+
+// Create all event listeners
+
+
+
+
+// Any additional functions
+
+//random word generator
+function createWord() {
+  
+return word;
+}
+alert(createWord());
+
+createWord();
+//what to do when out of moves
+function amountOfTurns() {
+  if (turnsLeft) {
+    alert('Nice work Law Dog');
+    return resetGame;
+  } else {
+    alert("You Saved The Day!");
+  }
+}
+
+// function chosenLetter(alphabet) {
+//   var picked 
+// }
+// function deincrementTurnCounter() {
+//   if () {
+// }
+// }
+
+
+
+// turns to start with = turns = 10
+// create a container with all the letters in alphabet = alphabet
+// letter choosen 
+// compare with array of letters in the phrase
+// if its there fill those spots
+// loop back to choose a letter
+// if not deincrement for incorrect guess and delete letter and add body part to hang
